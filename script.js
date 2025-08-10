42061430dfd9368b61e5191869ddf47e3a5f5d9a
AdiyahFarhan <adiyafarhan110@gmail.com>
2025-08-11 00:45:24 +0100

    Write a javaScript code to display the list of all episodes of a tv show project

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  allEpisodes.map(makePageForEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
+function makePageForEpisodes(episode) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  // Clone the template content
+  // and fill it with episode data
+  const episodeCard = document
+    .getElementById("episode-template")
+    .content.cloneNode(true);
+
+  // Set the episode title, image, and summary
+  // using the data from the episode
+  let epCode = episodeCode(episode.season, episode.number);
+  episodeCard.querySelector(".episode-title").textContent =
+    episode.name + " - " + epCode;
+  episodeCard.querySelector(".episode-image").src = episode.image.medium;
+  episodeCard.querySelector(".episode-summary").innerHTML = episode.summary;
+
+  // Append the episode card to the root element
+  rootElem.appendChild(episodeCard);
+}
+
+function episodeCode(season, episode) {
+  // Format the season and episode numbers to be two digits
+  // to create a code like "S01E01"
+  const code = `S${season.toString().padStart(2, "0")}E${episode
+    .toString()
+    .padStart(2, "0")}`;
+  return code;
 }
 
 window.onload = setup;
